966a54b17089edcc6752783c12cc7b19f7aa6986
Fahmi <pammimajid@gmail.com>
2020-03-12 21:37:22 +0800

    ok

---
+++ tugas_7_js.js
@@ -0,0 +1,13 @@
+var TinggiBadan =[165,170,175,176,177,180,190,200,210,215];
+for(var i=0; i<TinggiBadan.length;i++){
+	console.log(TinggiBadan[i])
+}
+
+console.log("")
+
+var TinggiBadan =[165,170,175,176,177,180,190,200,210,215]
+for (let x of TinggiBadan) {
+	console.log(x)
+	}
+
+//hasilnya tidak memiliki perbedaan
